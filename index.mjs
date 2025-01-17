ba186f6eccea2ef40928a791188f7391c3fa36f8
Kreliannn <krelianquimson@gmail.com>
2025-01-17 16:03:27 +0800

    integrate ai

--- index.mjs
+++ index.mjs
@@ -12,26 +12,21 @@ app.use(cors({
 //give a a short and simple description like in pokedex, the pokemon is charizard
 
 
+const genAI = new GoogleGenerativeAI("AIzaSyBdrJMVA-cG86Dj3dJIskhB0DsCbo7CwFk");
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
 app.post("/getPokemonData", async (request, response) => {
     let img = request.body.img
     let name = img
 
-    let description = "Charizard, a lroeakdsfjasfjaljskfjkasfkajlks jfaslkj fjkas fasjk fjklas jfaskj fa Fire/Flying-type Pokémon, is known for its dragon-like appearance and fiery breath. It flies high in the sky, breathing intense flames from its mouth. Charizard is fierce, powerful, and a strong protector of its trainer."
-    const stats = [
-        {stateName: 'hp', stat: 78},
-        {stateName: 'attack', stat: 84},
-        {stateName: 'defense', stat: 78},
-        {stateName: 'special-attack', stat: 109},
-        {stateName: 'special-defense', stat: 85},
-        {stateName: 'speed', stat: 100}
-    ]
-
-    let types = ["water", "fire"]
     
     try {
-            /*
-        let pokemon = "charizard"
+    
+        let pokemon = "lucario"
         let res = await axios.get("https://pokeapi.co/api/v2/pokemon/" + pokemon);
+
+        const result = await model.generateContent("give a a short and simple description like in pokedex, the pokemon is " + pokemon);
+
     
         let name = res.data.name
         let picture = res.data.sprites.other['official-artwork'].front_default
@@ -39,16 +34,17 @@ app.post("/getPokemonData", async (request, response) => {
         let types = res.data.types.map((obj) => obj.type.name )
         let stats = res.data.stats.map((obj) => ({stateName : obj.stat.name, stat : obj.base_stat}))
         let height = (res.data.height / 10)
-        let weight = res.data.weight */
+        let weight = res.data.weight 
+        let description = result.response.text()
         
      
      
         let pokemonData = {
-            name : "charizard",
-            picture : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-            sprite : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
-            height : (15 / 10),
-            weight : 900,
+            name : name,
+            picture : picture,
+            sprite : sprite,
+            height : height,
+            weight : weight,
             types : types,
             stats : stats,
             description : description
